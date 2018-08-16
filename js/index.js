2012831c2b9257123dcf1f6135e6bd48021133b3
2804011057 <2804011057@qq.com>
2018-08-16 10:07:52 +0800

    改变了一些东西

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 	//所有页面网址 ID
 
-	var url_all = 'http://192.168.3.12:8088';
-//	var url_all = 'http://47.101.33.180:8088';
+//	var url_all = 'http://192.168.3.12:8088';
+	var url_all = 'http://47.101.33.180:8088';
 
 	var tenantId = 'da9476e0b4424ff9a83ec8f2ac603e5e';
 
