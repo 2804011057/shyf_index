3b132bfeeb77e18f8f18c8b85b9671a8841eabab
2804011057 <2804011057@qq.com>
2018-12-10 15:19:18 +0800

    上传
    第一次

---
+++ js/plugin.js
@@ -0,0 +1,56 @@
+// document.addEventListener("plusready", function() {
+// 	var _BARCODE = 'MyPlugin',
+// 		B = window.plus.bridge;
+// 	var myPlugin = {
+// 	    /*连接蓝牙打印机*/
+// 		connectBluetoothPrinter: function(successCallback, errorCallback) {
+// 			var success = typeof successCallback !== 'function' ? null : function(args) {
+// 					successCallback(args);
+// 				},
+// 				fail = typeof errorCallback !== 'function' ? null : function(code) {
+// 					errorCallback(code);
+// 				};
+// 			callbackID = B.callbackId(success, fail);
+// 
+// 			return B.exec(_BARCODE, "connectBluetoothPrinter", [callbackID]);
+// 		},
+// 		/*打印图片*/
+// 		printImg: function(data, successCallback, errorCallback) {
+// 			var success = typeof successCallback !== 'function' ? null : function(args) {
+// 					successCallback(args);
+// 				},
+// 				fail = typeof errorCallback !== 'function' ? null : function(code) {
+// 					errorCallback(code);
+// 				};
+// 			callbackID = B.callbackId(success, fail);
+// 
+// 			return B.exec(_BARCODE, "PrintImg", [callbackID, data]);
+// 		},
+// 		/*打开蓝牙打印机*/
+// 		openBluetoothPrinter: function(data, successCallback, errorCallback) {
+// 			var success = typeof successCallback !== 'function' ? null : function(args) {
+// 					successCallback(args);
+// 				},
+// 				fail = typeof errorCallback !== 'function' ? null : function(code) {
+// 					errorCallback(code);
+// 				};
+// 			callbackID = B.callbackId(success, fail);
+// 
+// 			return B.exec(_BARCODE, "openBluetoothPrinter", [callbackID, data]);
+// 		},
+// 		/*蓝牙是否启用*/
+// 		bluetoothIsEnabled: function(successCallback, errorCallback) {
+// 			var success = typeof successCallback !== 'function' ? null : function(args) {
+// 					successCallback(args);
+// 				},
+// 				fail = typeof errorCallback !== 'function' ? null : function(code) {
+// 					errorCallback(code);
+// 				};
+// 			callbackID = B.callbackId(success, fail);
+// 
+// 			return B.exec(_BARCODE, "bluetoothIsEnabled", [callbackID]);
+// 		}
+// 
+// 	};
+// 	window.plus.myPlugin = myPlugin;
+// }, true);
